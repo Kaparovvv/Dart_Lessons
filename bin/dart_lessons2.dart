a9acda7907cc2da641e5a4efb51ec44c3f2ffe66
Kaparovvv <kaparovkanagat@gmail.com>
2022-03-14 19:43:20 +0600

    Hello World!

---
+++ bin/dart_lessons2.dart
@@ -0,0 +1,6 @@
+import 'package:dart_lessons2/dart_lessons2.dart' as dart_lessons2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_lessons2.calculate()}!');
+  print('qwerty');
+}
